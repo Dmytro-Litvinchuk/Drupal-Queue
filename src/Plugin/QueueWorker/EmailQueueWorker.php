19fade043469bf61ec92f665e4bf895a376f22c9
Dmytro-Litvinchuk <dimawaw11@gmail.com>
2020-03-03 11:41:12 +0200

    DI

--- src/Plugin/QueueWorker/EmailQueueWorker.php
+++ src/Plugin/QueueWorker/EmailQueueWorker.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\simple_batch\Plugin\QueueWorker;
 
-use Drupal\Core\Annotation\QueueWorker;
-use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Queue\QueueWorkerBase;
 use Drupal\user\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Process a queue.
@@ -16,25 +18,78 @@ use Drupal\user\Entity\User;
  *   cron = {"time" = 45}
  * )
  */
-class EmailQueueWorker extends QueueWorkerBase {
+class EmailQueueWorker extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\Mail\MailManagerInterface
+   */
+  protected $mailManager;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * EmailQueueWorker constructor.
+   *
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   * @param \Drupal\Core\Mail\MailManagerInterface $mailManager
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityManager
+   */
+  public function __construct(array $configuration,
+                              $plugin_id,
+                              $plugin_definition,
+                              MailManagerInterface $mailManager,
+                              EntityTypeManagerInterface $entityManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->mailManager = $mailManager;
+    $this->entityManager = $entityManager;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function create(ContainerInterface $container,
+                                array $configuration,
+                                $plugin_id,
+                                $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('plugin.manager.mail'),
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * @inheritDoc
    */
   public function processItem($data) {
-    $mailManager = \Drupal::service('plugin.manager.mail');
+    $mail = $this->mailManager;
     $params = $data;
-    $admin_users_id = \Drupal::service('entity_type.manager')
+    $adminUsersId = $this->entityManager
       ->getStorage('user')
       ->getQuery()
       ->condition('status', 1)
       ->condition('roles', 'administrator')
       ->execute();
-    foreach ($admin_users_id as $uid) {
+    // Get emails for all users with admin role.
+    foreach ($adminUsersId as $uid) {
       $user = User::load($uid);
-      $email = $user->getEmail();
-      $mailManager->mail('simple_batch', 'my_custom_email', $email, 'en', $params , $send = TRUE);
+      // Check exist.
+      if (!isset($to)) {
+        $to = $user->getEmail();
+      }
+      else {
+        // Return string value (example@gmail, some@mail.com)
+        $to .= ', ' . $user->getEmail();
+      }
     }
+    $mail->mail('simple_batch', 'my_custom_email', $to, 'en', $params, TRUE);
   }
 
 }
