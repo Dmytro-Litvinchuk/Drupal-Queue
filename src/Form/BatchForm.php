b6822fa94b0633787baa5dd14d7a1eae154aa646
Dmytro-Litvinchuk <dimawaw11@gmail.com>
2020-03-18 11:15:23 +0200

    manual

--- src/Form/BatchForm.php
+++ src/Form/BatchForm.php
@@ -4,10 +4,43 @@ namespace Drupal\simple_batch\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Queue\QueueFactory;
+use Drupal\Core\Queue\QueueWorkerManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ *
+ */
 class BatchForm extends FormBase {
 
+  /**
+   * @var QueueFactory
+   */
+  protected $queueFactory;
 
+  /**
+   * @var QueueWorkerManagerInterface
+   */
+  protected $queueManager;
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(QueueFactory $queue, QueueWorkerManagerInterface $queue_manager) {
+    $this->queueFactory = $queue;
+    $this->queueManager = $queue_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('queue'),
+      $container->get('plugin.manager.queue_worker')
+    );
+  }
 
   /**
    * @inheritDoc
@@ -20,19 +53,17 @@ class BatchForm extends FormBase {
    * @inheritDoc
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $queue = \Drupal::queue('my_custom_email');
-    if ($numberOfItems = $queue->numberOfItems()) {
-      $form['info'] = [
-        '#type' => 'inline_template',
-        '#template' => '<div class="info-form"><h3>This queue is already running.</h3></div>',
-      ];
-    }
-    else {
-      $form['submit'] = [
-        '#type' => 'submit',
-        '#value' => $this->t('Run batch'),
-      ];
-    }
+    $queue = $this->queueFactory->get('my_custom_email');
+    $form['help'] = [
+      '#type' => 'markup',
+      '#markup' => $this->t('Submitting this form will process the Manual Queue which contains @number items.', ['@number' => $queue->numberOfItems()]),
+    ];
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Process queue'),
+      '#button_type' => 'primary',
+    ];
     return $form;
   }
 
@@ -40,7 +71,21 @@ class BatchForm extends FormBase {
    * @inheritDoc
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    //$queue = \Drupal::queue('my_custom_email');
+    $queue = $this->queueFactory->get('my_custom_email');
+    $queue_worker = $this->queueManager->createInstance('my_custom_email');
+    while ($item = $queue->claimItem()) {
+      try {
+        $queue_worker->processItem($item->data);
+        $queue->deleteItem($item);
+      }
+      catch (SuspendQueueException $e) {
+        $queue->releaseItem($item);
+        break;
+      }
+      catch (\Exception $e) {
+        watchdog_exception('npq', $e);
+      }
+    }
   }
 
 }
